552c353a536521d5db0fab91e9afb28749aa2c04
Daniel Wei <dominum15@outlook.com>
2019-04-22 22:38:35 -0500

    Add all files

---
+++ src/Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+    /**
+     * Run the program
+     * Feel free to modify howCloselyMatchesInput
+     * @param args
+     */
+    public static void main(String[] args) {
+        int howCloselyMatchesInput = 7;
+        String[] input = {howCloselyMatchesInput+"", "10000", "input.txt"};
+        TextGenerator.main(input);
+    }
+}
